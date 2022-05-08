a9298387eebe3d0f9e2cd70c0dee39dc8df56629
Zohan Marediya <marediya.zohan99@gmail.com>
2022-05-08 16:02:09 -0500

    Added a responsive Contact section

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import About from "./components/About";
 import Projects from "./components/Projects";
+import Contact from "./components/Contact";
 
 //External Imports
 
@@ -16,6 +17,7 @@ function App() {
       <Hero />
       <About />
       <Projects />
+      <Contact />
     </div>
   );
 }
