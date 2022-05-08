490130c298c6a35f9e83a6ac3142d9ae4f1c0af8
Zohan Marediya <marediya.zohan99@gmail.com>
2022-05-08 16:00:59 -0500

    Fixed resizing issue where component would got out of window

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -20,17 +20,27 @@ function ProjectCard({project}){
                 component={"img"}
                 image={project["image_path"]}
                 alt={project["title"]}
-                sx={{maxWidth: "95%", ml: "auto", mr: "auto", borderRadius: 8}}
+                sx={{maxWidth: "95%", mx:"auto", borderRadius: 8}}
             />
             <CardContent>
-                <Link 
-                    href={project["link"]}
+                {project.websiteLink !== "" && <Link 
+                    href={project["websiteLink"]}
                     underline={"hover"}
                     target="_blank"
                     rel="noreferrer"
                     fontSize={"1.25rem"}
                 >
-                    {project["link"]}
+                    Website Link
+                </Link>}
+                {project.websiteLink !== "" && <br />}
+                <Link 
+                    href={project["gitLink"]}
+                    underline={"hover"}
+                    target="_blank"
+                    rel="noreferrer"
+                    fontSize={"1.25rem"}
+                >
+                    Github Link
                 </Link>
                 <Typography
                     mt={2}
@@ -43,10 +53,11 @@ function ProjectCard({project}){
     );
 }
 
-function projectElem(title, link, description, image_path){
+function projectElem(title, gitLink, websiteLink, description, image_path){
     return {
         "title": title,
-        "link": link,
+        "gitLink": gitLink,
+        "websiteLink": websiteLink,
         "description": description,
         "image_path": image_path
     };
@@ -55,10 +66,12 @@ function projectElem(title, link, description, image_path){
 function addProjects(projectList){
     const artemisSystem = projectElem("Artemis Observation System (An Embedded Forest Fire Detection System)",
     "http://github.com/LoRaWAN-Forest-Fire-Detection",
+    "",
     "This was an embedded system project with a web application component for an easier access to data for the users. the aim of this project was to build a system to detect fire in remote locations without access to wifi. This project was built with python for the nodes placed in the forest (where the sensors collected the data), and React for the web application. We used google api to display the node location and its real time status. The data transfer from the nodes was ddone with teh LoRaWAN protocol and DynamoDB was used for data storage needs.",
     "images/artemis.png");
 
     const sdw = projectElem("SDW (A Prototype HaaS System)",
+    "https://github.com/SuperDopeWebsiteTeam",
     "http://superdopewebsite.herokuapp.com/",
     "This was a demo website for how a potential Haas(Hardware as a Service) system would look like and function. It was built wth React as the frontend, python for the backend, and MongoDB for the database. Git and Agile was extensively used during the completion of the project for source control and efficient collaboration among teammembers.",
     "images/sdw.jpg");
