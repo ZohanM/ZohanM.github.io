01775ac22b9c2f36383e57c6e2dcff56d7c7e3d8
Zohan Marediya <marediya.zohan99@gmail.com>
2022-05-09 18:41:16 -0500

    Added footer section and theme to the website

---
+++ src/components/Footer.js
@@ -0,0 +1,20 @@
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+import {HashLink as Link} from "react-router-hash-link";
+
+function Footer(){
+    return(
+        <Box sx={{backgroundColor: "black", height: "20vh"}}>
+            <Link to="/#top" smooth>
+                <Typography sx={{textDecoration: "none", color: "primary.main", "&:visited": {color: "inherit"}}}>
+                    Go to top
+                </Typography>
+            </Link>
+            <Typography variant="subtitle2" color={"white"} sx={{mt: "15vh"}}>
+                Designed and developed by Zohan Marediya
+            </Typography>
+        </Box>
+    );
+}
+
+export default Footer;
